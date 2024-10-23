fc7eee2e9d4331f275092becb2c0d755058b6ada
HenriqueSim2 <hsnicolo3@gmail.com>
2024-10-23 14:23:28 -0300

    mais atualizado 14:23 23/10

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'; 
 import Aurea from './site-aurea/index.jsx';
+import Anel from './site-aurea/site-aure-parte-joias/anel.jsx'
 
 export function Navigation() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path='/' element={<Aurea />} />
+                <Route path='/anel' element={<Anel />} />
             </Routes>
         </BrowserRouter>
     )
