fb421559963aae38adf8caf89709b5b4d3019c70
HenriqueSim2 <hsnicolo3@gmail.com>
2024-10-24 13:59:01 -0300

    commits

--- src/site-aurea/site-aure-parte-joias/anel.jsx
+++ src/site-aurea/site-aure-parte-joias/anel.jsx
@@ -7,25 +7,25 @@ export default function Anel() {
     return (
         <div className='cardsgeral'>
             <div className='parte1'>
-            <div className='cards'>
-                <div><p className='texto'>
-                    <img className='imagg' src="anell.jpg" alt="" /><br />
+            <div className='cards2'>
+                <div><p className='texto22'>
+                    <img className='imaggg' src="anell.jpg" alt="" /><br />
                     PULSEIRA   RETANGULAR CRAVEJADA FOLEADA A OURO 18K         <br /><br />
                     R$  ̶ ̶4̶0̶0̶,̶0̶0̶        <br />
                     R$ 340,00        </p>
                 </div>
             </div>
-            <div className='cards'>
-                <div><p className='texto'>
-                    <img className='imagg' src="anell.jpg" alt="" /><br />
+            <div className='cards2'>
+                <div><p className='texto22'>
+                    <img className='imaggg' src="anell.jpg" alt="" /><br />
                     PULSEIRA   RETANGULAR CRAVEJADA FOLEADA A OURO 18K         <br /><br />
                     R$  ̶ ̶4̶0̶0̶,̶0̶0̶        <br />
                     R$ 340,00        </p>
                 </div>
             </div>
-            <div className='cards'>
-                <div><p className='texto'>
-                    <img className='imagg' src="anell.jpg" alt="" /><br />
+            <div className='cards2'>
+                <div><p className='texto22'>
+                    <img className='imaggg' src="anell.jpg" alt="" /><br />
                     PULSEIRA   RETANGULAR CRAVEJADA FOLEADA A OURO 18K         <br /><br />
                     R$  ̶ ̶4̶0̶0̶,̶0̶0̶        <br />
                     R$ 340,00        </p>
@@ -34,25 +34,25 @@ export default function Anel() {
             </div>
             
             <div className='parte2'>
-                <div className='cards'>
-                <div><p className='texto'>
-                    <img className='imagg' src="anell.jpg" alt="" /><br />
+                <div className='cards2'>
+                <div><p className='texto22'>
+                    <img className='imaggg' src="anell.jpg" alt="" /><br />
                     PULSEIRA   RETANGULAR CRAVEJADA FOLEADA A OURO 18K         <br /><br />
                     R$  ̶ ̶4̶0̶0̶,̶0̶0̶        <br />
                     R$ 340,00        </p>
                 </div>
             </div>
-            <div className='cards'>
-                <div><p className='texto'>
-                    <img className='imagg' src="anell.jpg" alt="" /><br />
+            <div className='cards2'>
+                <div><p className='texto22'>
+                    <img className='imaggg' src="anell.jpg" alt="" /><br />
                     PULSEIRA   RETANGULAR CRAVEJADA FOLEADA A OURO 18K         <br /><br />
                     R$  ̶ ̶4̶0̶0̶,̶0̶0̶        <br />
                     R$ 340,00        </p>
                 </div>
         </div>
-        <div className='cards'>
-                <div><p className='texto'>
-                    <img className='imagg' src="anell.jpg" alt="" /><br />
+        <div className='cards2'>
+                <div><p className='texto22'>
+                    <img className='imaggg' src="anell.jpg" alt="" /><br />
                     PULSEIRA   RETANGULAR CRAVEJADA FOLEADA A OURO 18K         <br /><br />
                     R$  ̶ ̶4̶0̶0̶,̶0̶0̶        <br />
                     R$ 340,00        </p>
