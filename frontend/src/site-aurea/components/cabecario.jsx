d92f7a41e4c5698083457b766d8068f55f9515d9
HenriqueSim2 <hsnicolo3@gmail.com>
2024-10-31 18:23:22 -0300

    pronto por hj

--- frontend/src/site-aurea/components/cabecario.jsx
+++ frontend/src/site-aurea/components/cabecario.jsx
@@ -5,8 +5,10 @@ import './cabecario.scss';
 
 export default function Cabecario() {
 return (
+<div className='todo'>
     <div className='cabecario'>
         <img className='auria' src="auria.png" alt="" />
     </div>
+</div>
 );
 }
